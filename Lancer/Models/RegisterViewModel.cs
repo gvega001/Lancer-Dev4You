3228da3ed73c110c0d40f597556b10683b902d2e
Gabriel Vega <gvega001@gmail.com>
2020-07-25 00:58:05 -0700

    Connection made. Not caught by my dbException.

--- Lancer/Models/RegisterViewModel.cs
+++ Lancer/Models/RegisterViewModel.cs
@@ -18,7 +18,7 @@ namespace Lancer.Models
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-
+        [Required]
         [Compare("Password", ErrorMessage = "Passwords must match")]
         [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
